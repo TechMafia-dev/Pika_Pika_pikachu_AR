506b61743a11abb1ce157137fb25a15856415e15
TechMafia-dev <techmafia.sp@gmail.com>
2022-07-07 00:48:52 +0530

    v1.1 - AR intro try2

---
+++ easylearnar/src/main/java/com/example/easylearnar/MainActivity_EasyLearn_AR.java
@@ -0,0 +1,14 @@
+package com.example.easylearnar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_EasyLearn_AR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_easy_learn_ar);
+    }
+}
